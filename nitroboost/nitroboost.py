f1b1f15a65260289b62f351c90359113282c5d62
leviyang <99230315+leviyang@users.noreply.github.com>
2022-07-01 12:06:11 +0900

    Update nitroboost.py

--- nitroboost/nitroboost.py
+++ nitroboost/nitroboost.py
@@ -9,7 +9,10 @@ class BoostPlugin(commands.Cog):
 
         print(message.type)
         if message.type == discord.MessageType.premium_guild_subscription:
-            embed = discord.Embed(title=f"**Nitro Boost**", description=f"Thank you so much for boosting <a:BoostingAnimated:707512475246919712> !", color=0xff0000)
+            embed = discord.Embed(title=f"**Ding ding! We got a new booster!**", description=f"Thank you so much for boosting ﹫: s. effects. ˀ ⸝⸝ ％! Our server perks are pinned in the pinned messages! <3.
+", color=0xff0000)
+                embed.set_thumbnail(url="https://i.pinimg.com/564x/1e/41/7c/1e417c8e8fbe94dd90ba8e3244a2acf2.jpg")
+                embed.set_footer(text="perks are in the pinned msg in this channel :) dm owner to claim.")
             m = await message.channel.send(embed=embed)
             await m.add_reaction("<a:Heartanimated:707510241276985425>")
             
